c9e2a6acf98db3bf464fc0a7bd9bfa8a91b67765
mchatti <mchatti@genymobile.com>
2023-06-19 09:56:32 +0200

    Fix troubles to push ssl custom cert on android 10 unrooted device
    Update dockerfile by removing all unnecessary stuff
    
    Update work script: adding adb rooting command and making it simpler
    
    Update README file according to previous changes

--- Dockerfile
+++ Dockerfile
@@ -1,37 +1,11 @@
-# SSL Certificate generation with let's encrypt
+FROM ubuntu:22.04
 
-# Before running this, your <domain> DNS should already point to your
-# AWS instance IP and your AWS security group should allow let's encrypt servers
-
-# HOWTO
-# git clone https://github.com/genymobile/aws-ssl/
-# cd aws-ssl
-# put your own genymotion_aws.pem file there (or symlink it)
-# build this docker:
-    # docker build -t <usr>/<name> .
-
-# To use existing certificate
-# docker run --net=host -P -v <path_to_aws_ssl>:/opt/work/ --env domain=<domain> --env package=true --env keystorepassword=<kspassword> --env certpassword=<kppassword> -t -i <usr>/<name> /opt/work/package.sh <relpath_to_privkey.pem> <relpath_to_fullchain.pem>
-
-# Alternatively, you could install those dependencies on your own distro, set environnment variables
-# according to the 'docker run' line and run generate.sh
-
-FROM ubuntu:16.04
-
-RUN echo "deb http://archive.ubuntu.com/ubuntu/ trusty-security multiverse" >> /etc/apt/sources.list && apt update
-
-RUN apt install -y \
+RUN apt-get -qq update && \
+    apt-get install -y \
         ssh \
         default-jdk \
         android-tools-adb \
-        python python-dev \
-        gcc \
-        libssl-dev \
-        openssl \
-        libffi-dev \
-        ca-certificates \
-        virtualenv
+        openssl
 
-EXPOSE 8888
-
-ENTRYPOINT ["/bin/bash"]
+WORKDIR /opt/work
+ENTRYPOINT ./package.sh privkey.pem fullchain.pem
--- package.sh
+++ package.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 set -x
-if [ -n "${package}" ] ; then
-    pushd /opt/work/
-    privkey=$1
-    fullchain=$2
-    ssh -oServerAliveInterval=1 -oServerAliveCountMax=2 -oStrictHostKeyChecking=no -N -i genymotion_aws.pem -L 5555:localhost:5555 root@${domain} &
 
-    # Wait for ssh ...
-    sleep 5
-else
-    privkey=/etc/letsencrypt/live/${domain}/privkey.pem
-    fullchain=/etc/letsencrypt/live/${domain}/fullchain.pem 
+print_usage_and_exit () {
+        echo "usage: $0 <privkey> <fullchain>"
+        exit 1
+}
 
+if [ -z "$1" ]; then
+	print_usage_and_exit
 fi
+
+if [ -z "$2" ]; then
+	print_usage_and_exit
+fi
+
+privkey=$1
+fullchain=$2
+
+if [ ! -s ${privkey} ]; then
+	echo "${privkey} is empty or not found"
+	exit 1
+fi
+
+if [ ! -s ${fullchain} ]; then
+	echo "${fullchain} is empty or not found"
+	exit 2
+fi
+
 # Generate temporary PKCS12
-openssl pkcs12 -export -inkey ${privkey} -in ${fullchain} -out tmp.pkcs12 -name genymotion -password pass:shortlivedpassword
+openssl pkcs12 -export -inkey ${privkey} -in ${fullchain} -out tmp.pkcs12 -name genymotion -password pass:shortlivedpassword -passin pass:${keystorepassword}
 
 # Generate BouncyCastle keystore from PKCS12
 # First fetch the BouncyCastle provider that'll be used by 'keytool'
@@ -27,9 +41,14 @@ rm tmp.pkcs12
 rm bcprov-jdk15on-156.jar
 
 # Hardwork is done, give useful stuff to Android
-adb shell setprop persist.tls.pw.ks ${keystorepassword}
-adb shell setprop persist.tls.pw.cert ${certpassword}
-adb shell mkdir /data/misc/tls/
+adb connect ${domain}:5555
+sleep 1
+adb root
+adb shell mkdir -p /data/misc/tls/
 adb push custom.bks /data/misc/tls/custom.bks
 adb shell sync
+adb shell setprop persist.tls.pw.ks ${keystorepassword}
+adb shell setprop persist.tls.pw.cert ${certpassword}
 
+# A last clean up
+rm custom.bks
